6f5edcd833500a8b58713d6dafc4289cecc8e97a
ana <saraiva.anap@gmail.com>
2024-02-27 18:26:18 -0300

    fix tabela

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -3,7 +3,7 @@ import { Card, Table } from 'react-bootstrap';
 import './Dashboard.css';  // Importando o arquivo CSS
 
 const Dashboard = () => {
-  // Dados estáticos fictícios
+  // Dados estáticos fictícios para a primeira tabela
   const salesData = [
     { id: 1, product: 'Planilha A', amount: 1500 },
     { id: 2, product: 'Planilha B', amount: 2000 },
@@ -11,30 +11,65 @@ const Dashboard = () => {
     // Adicione mais dados conforme necessário
   ];
 
+  // Dados estáticos fictícios para a segunda tabela
+  const otherData = [
+    { id: 1, item: 'Item 1', quantity: 5 },
+    { id: 2, item: 'Item 2', quantity: 10 },
+    { id: 3, item: 'Item 3', quantity: 8 },
+    // Adicione mais dados conforme necessário
+  ];
+
   return (
-    <Card className="Dashboard">
-      <h2>Relatórios</h2>
-      <Card.Body>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>ID</th>
-              <th>Planilha</th>
-              <th>Grau Crítico</th>
-            </tr>
-          </thead>
-          <tbody>
-            {salesData.map((sale) => (
-              <tr key={sale.id}>
-                <td>{sale.id}</td>
-                <td>{sale.product}</td>
-                <td>{sale.amount}</td>
+    <div>
+      <h2>Planilha #01</h2>
+      <Card className="Dashboard">
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Resultado</th>
+                <th>COD</th>
+                <th>Indicadores</th>
+                <th>Grau Crítico</th>
               </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Card.Body>
-    </Card>
+            </thead>
+            <tbody>
+              {salesData.map((sale) => (
+                <tr key={sale.id}>
+                  <td>{sale.product}</td>
+                  <td>{sale.id}</td>
+                  <td>{sale.product}</td>
+                  <td>{sale.amount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+
+      <Card className="Dashboard">
+        <Card.Body>
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>RESULTADO</th>
+                <th>CONTAGEM DE COD</th>
+                <th>%</th>
+              </tr>
+            </thead>
+            <tbody>
+              {otherData.map((data) => (
+                <tr key={data.id}>
+                  <td>{data.item}</td>
+                  <td>{data.id}</td>
+                  <td>{data.quantity}</td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Card.Body>
+      </Card>
+    </div>
   );
 }
 
