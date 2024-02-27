8a69e2df96aa752aa88c786dd2ef5475db81f334
ana <saraiva.anap@gmail.com>
2024-02-27 19:33:00 -0300

    fix sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,4 +1,3 @@
-// Sidebar.js
 import React from 'react';
 import { Card } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
@@ -9,11 +8,15 @@ const Sidebar = () => {
     <Card className="Sidebar">
       <Card.Body>
         <ul>
-          <li><Link to="/pagina_inicial">requisito 1</Link></li>
-          <li><Link to="/pagina_inicial">requisito 2</Link></li>
+          <li><Link to="/pagina_inicial">Requisito 1</Link></li>
+          <li><Link to="/pagina_inicial">Requisito 2</Link></li>
           <li><Link to="/pagina_inicial">Resultado</Link></li>
           {/* Adicione mais links conforme necessário */}
         </ul>
+        <div className="SidebarBottom">
+          <button className="LogoutButton">Sair</button>
+        </div>
+        <h3>© NPCA</h3>
       </Card.Body>
     </Card>
   );
